3f66db06a810742cc8704d16a955914cc16e5e25
Danny Juergens <djuergen@ucsd.edu>
2019-11-30 13:02:02 -0800

    minor changes to btn listeners

--- xmaswishlist.js
+++ xmaswishlist.js
@@ -1,6 +1,17 @@
 let addBtn = document.getElementById('add');
+let cancelBtn = document.getElementById('cancel');
+let saveBtn = document.getElementById('save');
 let dialog = document.getElementById('add-dialog');
 
 addBtn.addEventListener('click', function showDialog(){
     dialog.open = true;
+});
+
+cancelBtn.addEventListener('click', function hideDialog(){
+    dialog.open = false;
+});
+
+saveBtn.addEventListener('click', function saveData(){
+    // TODO : save data to firebase db
+    dialog.open = false;
 });
