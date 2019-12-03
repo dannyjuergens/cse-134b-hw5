02460a667972962309888266ad4ecbdb39f3c472
Danny Juergens <djuergen@ucsd.edu>
2019-12-02 18:10:39 -0800

    updated db logic and todo

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cse-134b-hw5
 Assignment 5 for CSE 134B FA19
+
+
+TODO
+[x] rewrite firestore to be users > wishlist > data 
+[x] code path to go thru user
+[x] add to db collection under each user
+[x] pull data to put in UL 
+[x] pull by grabbing user doc and iterating thru 
+[ ] take data and put into HTML
+[ ] add edit and delete buttons for each
+[ ] hook up edit and delete buttons to existing functions
+[ ] style
+[ ] log out logic
+ */ 
--- xmaswishlist.js
+++ xmaswishlist.js
@@ -1,33 +1,52 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
-
+firebase.initializeApp({
+    apiKey: "AIzaSyAR0tzJM0wkVPRKT8Ax34YJE_nQcbZkxA0",
+    authDomain: "cse134bhw5-cc389.firebaseapp.com",
+    databaseURL: "https://cse134bhw5-cc389.firebaseio.com",
+    projectId: "cse134bhw5-cc389",
+    storageBucket: "cse134bhw5-cc389.appspot.com",
+    messagingSenderId: "491932596441",
+    appId: "1:491932596441:web:17c370655e264224d566c8",
+    measurementId: "G-8S6VFYHG3G"
+  });
 var db = firebase.firestore();
+const currentUserID = firebase.auth().W;
 
 let addBtn = document.getElementById('add');
 let cancelBtn = document.getElementById('cancel');
 let saveBtn = document.getElementById('save');
 let dialog = document.getElementById('add-dialog');
 
+let form = document.getElementById('form');
 let name = document.getElementById('name');
 let photo = document.getElementById('item-pic');
 let desc = document.getElementById('description');
 let price = document.getElementById('price');
 let category = document.getElementById('category');
 
-let wishlistDB = db.collection('wishlist');
+
+function getWishlist() {
+    let wishlist = db.collection(`users/${currentUserID}/wishlist`).get().then(function(querySnapshot){
+        querySnapshot.forEach(function(doc){
+            let item = doc.data();
+            // load item into html 
+        });
+    });
+    console.log(wishlist);
+}
 
 addBtn.addEventListener('click', function showDialog(){
+    form.reset();
     dialog.open = true;
 });
 
 cancelBtn.addEventListener('click', function hideDialog(){
+    form.reset();
     dialog.open = false;
 });
 
 saveBtn.addEventListener('click', function saveData(){
     // save data to firebase db
-    wishlistDB.doc('name').set({
+    db.collection(`users/${currentUserID}/wishlist`).doc(`${name.value}`).set({
         name: name.value,
         photo: photo.value,
         desc: desc.value,
@@ -42,3 +61,15 @@ saveBtn.addEventListener('click', function saveData(){
     });
     dialog.open = false;
 });
+
+function deleteItem(itemName){
+    db.collection(`users/${currentUserID}/wishlist`).doc(`${itemName}`).delete().then(function() {
+        console.log("Document successfully deleted!");
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    })
+}
+
+function editItem(){
+    dialog.open = true;
+}
